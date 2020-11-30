8d464a9cd0ec6485802b434c98be9a570d86bcb9
Dion <ksundong@gmail.com>
2020-11-30 22:51:56 +0900

    [#10][hotfix]ingress 포트 설정 3306으로 수정

--- vpc/rds.tf
+++ vpc/rds.tf
@@ -40,8 +40,8 @@ resource "aws_security_group" "bladi_todo_db_sg" {
   vpc_id = aws_vpc.bladi_todo_vpc.id
 
   ingress {
-    from_port = 0
-    to_port = 0
+    from_port = 3306
+    to_port = 3306
     protocol = "tcp"
     security_groups = [
       aws_security_group.bladi_todo_web_server_sg.id
